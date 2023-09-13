69a934c527f012d416239d2c95a0ce8d04cade39
Martin Mensch <martin.pessoa@gmail.com>
2023-09-12 23:59:09 -0300

    implementa os requerimentos

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+class Usuario(val nome: String) {
+    private val formacoes = mutableListOf<Formacao>()
+    fun matriculado(formacao: Formacao) {
+        formacoes.add(formacao)
     }
 }
 
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+    private val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach { usuario ->
+            usuario.matriculado(this)
+            inscritos.add(usuario)
+        }
+    }
+}
+
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudosLamp = listOf(
+        ConteudoEducacional("MySql Básico - CRUD", 30, Nivel.BASICO),
+        ConteudoEducacional("MySql Intermediário - Joins", 60, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("MySql Avançado - Instalando MySql no Linux", 90, Nivel.AVANÇADO),
+
+        ConteudoEducacional("Linux - instalando o LAMP STACK", 30, Nivel.BASICO),
+        ConteudoEducacional("Linux - configurando corretamente o LAMP", 60, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("Linux - manutenção e administração do seu servidor", 90, Nivel.AVANÇADO),
+
+        ConteudoEducacional("PHP - Quando usar o cifrão - $", 30, Nivel.BASICO),
+        ConteudoEducacional("PHP - Symfony, Laravel, CodeIgniter e Cake", 60, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("PHP - Legacy: aprenda a manter sites antigos em PHP 5 ou menos", 90, Nivel.AVANÇADO),
+
+        ConteudoEducacional("Apache - Instalando", 10, Nivel.BASICO),
+        ConteudoEducacional("Apache - Configurando seu primeiro site (copie nosso .htaccess)", 15, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("Apache - Aprendendo a ler a documentação", 90087, Nivel.AVANÇADO),
+    )
+
+    val formacaoLampStack = Formacao("LAMP Stack", conteudosLamp)
+
+    formacaoLampStack.matricular(Usuario("Martin V. Mensch"))
 }
